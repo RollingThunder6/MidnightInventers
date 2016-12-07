9ee47258b44b3dc3c60ca059ca79e9b7c4d91677
Arun Pottekat <apottekat@gmail.com>
2016-12-07 09:38:54 +0530

    Entropy based mechanism

--- Source Code/Entropy/entropy.py
+++ Source Code/Entropy/entropy.py
@@ -9,37 +9,55 @@ Author - Midnight Inventers
 
 fh = open("dummy.txt")
 
-i = 0
 flows = []
-rp_local = []
 N = []
 
-for line in fh:
-	if i != 0:
-		line = line.strip().split(",")
+def generate_flows():
+	global flows
 
-		# [ Filtering src ip and dst ip from flow table ]
-		# src_ip = line[13].split("=")
-		# dst_ip = line[14].split("=")
+	fh = open("dummy.txt")
+	i = 0
+	for line in fh:
+		if i != 0:
+			line = line.strip().split(",")
+			# [ Filtering src ip and dst ip from flow table ]
+			# src_ip = line[13].split("=")
+			# dst_ip = line[14].split("=")
 
-		# [ Adding rp_local to flow entry depending on whether host is present or not ]
-		# if src_ip[1] in switch_ip and dst_ip[1] in switch_ip:	
-		# 	rp_local.append(0)
-		# else:
-		# 	rp_local.append(-1)
+			# [ Adding rp_local to flow entry depending on whether host is present or not ]
+			# if src_ip[1] in switch_ip and dst_ip[1] in switch_ip:	
+			# 	rp_local.append(0)
+			# else:
+			# 	rp_local.append(-1)
 
-		# [ rp_local attribute added without checking ]
-		rp_local.append(0)
-		flows.append(line)
-	i = i + 1
+			# [ rp_local attribute added without checking ]
+			if line[-1:][0] != True:
 
-# packet count calculation for delta_t
-i = 0
-for flow in flows:
-	received_packets = flow[3].split("=")
-	received_packets = int(received_packets[1])
+				# rp_local added in flow entry
+				line.append(0)
+				line.append(True)
 
-	num_packets = received_packets - rp_local[i]
-	rp_local[i] = received_packets
-	i = i + 1
-	N.append(num_packets)
+			flows.append(line)
+		i = i + 1			
+
+def main():
+	# packet count calculation for delta_t
+	global N
+	i = 0
+	m = 0
+	w = 5
+
+	while True:
+		generate_flows()
+
+		for flow in flows:
+			received_packets = flow[3].split("=")
+			received_packets = int(received_packets[1])
+
+			num_packets = received_packets - rp_local[i]
+			rp_local[i] = received_packets
+			i = i + 1
+			N.append(num_packets)
+
+if __name__ == '__main__':
+	main()
