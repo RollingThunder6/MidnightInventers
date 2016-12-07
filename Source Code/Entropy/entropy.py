03bfc6d2299e3612b82effa881733ab3d1c42508
Arun Pottekat <apottekat@gmail.com>
2016-12-07 10:58:13 +0530

    Entropy based mechanism

--- Source Code/Entropy/entropy.py
+++ Source Code/Entropy/entropy.py
@@ -12,14 +12,36 @@ fh = open("dummy.txt")
 flows = []
 N = []
 
+flows_temp = {}
+
+# Hash function by indexing based on src + dst + protocol
+def hash_function(flow_entry):
+	global flows_temp
+
+	flow_entry = flow_entry.strip().split(",")
+	
+	src_ip = flow_entry[13].split("=")
+	dst_ip = flow_entry[14].split("=")
+	
+	src_ip_val = src_ip[1].split(".")
+	dst_ip_val = dst_ip[1].split(".")
+	
+	src = "".join(src_ip_val)
+	dst = "".join(dst_ip_val)
+	
+	val = src + dst + flow_entry[8]	
+	
+	flows_temp[val] = flow_entry
+
 def generate_flows():
-	global flows
 
 	fh = open("dummy.txt")
 	i = 0
 	for line in fh:
 		if i != 0:
-			line = line.strip().split(",")
+			hash_function(line)
+
+
 			# [ Filtering src ip and dst ip from flow table ]
 			# src_ip = line[13].split("=")
 			# dst_ip = line[14].split("=")
@@ -31,33 +53,63 @@ def generate_flows():
 			# 	rp_local.append(-1)
 
 			# [ rp_local attribute added without checking ]
-			if line[-1:][0] != True:
+			
 
-				# rp_local added in flow entry
-				line.append(0)
-				line.append(True)
 
-			flows.append(line)
+
+
+
+			# if line[-1:][0] != True:
+
+			# 	# rp_local added in flow entry
+			# 	line.append(0)
+			# 	line.append(True)
+
+			# flows.append(line)
 		i = i + 1			
 
 def main():
 	# packet count calculation for delta_t
-	global N
-	i = 0
-	m = 0
-	w = 5
 
-	while True:
-		generate_flows()
 
-		for flow in flows:
-			received_packets = flow[3].split("=")
-			received_packets = int(received_packets[1])
 
-			num_packets = received_packets - rp_local[i]
-			rp_local[i] = received_packets
-			i = i + 1
-			N.append(num_packets)
+
+
+
+
+
+
+	# global N
+	# i = 0
+	# m = 0
+	# w = 5
+
+	# while True:
+	# 	generate_flows()
+
+	# 	for flow in flows:
+	# 		received_packets = flow[3].split("=")
+	# 		received_packets = int(received_packets[1])
+
+	# 		num_packets = received_packets - rp_local[i]
+	# 		rp_local[i] = received_packets
+	# 		i = i + 1
+	# 		N.append(num_packets)
+
+
+
+
+
+
+
+
+	global flows_temp
+
+
+	generate_flows()
+
+	print(flows_temp)
+
 
 if __name__ == '__main__':
 	main()
