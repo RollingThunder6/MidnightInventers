e89e806d1263c6db405d87af5958f3f44f9658d2
Arun Pottekat <apottekat@gmail.com>
2017-01-03 09:15:34 +0530

    Flask application - Affected node not displayed

--- Source Code/Flask/app/static/js/devices.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".click-dropdown-field").click(function(event) {
-		$(this).children('.content').slideToggle(300, function() {
-			console.log("Slide event success.")
-		})
-	});
-});
