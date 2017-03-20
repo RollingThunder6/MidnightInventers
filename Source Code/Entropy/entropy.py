0fb4c8b6e5d9bf7e9345542a80f9e7b203690d1a
Arun Pottekat <apottekat@gmail.com>
2017-03-20 21:56:08 +0530

    Fuzzy C Means Algorithm completed

--- Source Code/Entropy/entropy.py
+++ Source Code/Entropy/entropy.py
@@ -19,7 +19,7 @@ hash_flows = {}
 def hash_function():
 	global hash_flows	
 
-	system("sudo ovs-ofctl dump-flows s1 > dummy.txt")
+	system("sudo ovs-ofctl dump-flows tcp:127.0.0.1:6634 > dummy.txt")
 	try:
 		fh = open("dummy.txt")
 	except FileNotFoundError as e:
@@ -29,7 +29,7 @@ def hash_function():
 	if hash_flows == {}:
 		i = 0
 		for flow_entry in fh:
-			if i > 3:
+			if i != 0:
 				flow_entry = flow_entry.strip().split(",")
 				
 				src_ip = flow_entry[13].split("=")
@@ -41,7 +41,7 @@ def hash_function():
 				src = "".join(src_ip_val)
 				dst = "".join(dst_ip_val)
 				
-				val = src + "/" + dst + flow_entry[8]		
+				val = src + "/" + dst + flow_entry[8]
 
 				# [ Filtering src ip and dst ip from flow table ]
 				# src_ip = line[13].split("=")
@@ -103,7 +103,7 @@ def main():
 	delta_t = 2
 	threshold = 0
 	standard_entropy = 0.5
-	alpha = [0.04, 0.11, 0.19, 0.28, 0.38	]
+	alpha = [0.04, 0.11, 0.19, 0.28, 0.38]
 	deviation = 0
 	multiplication_factor = 1
 
